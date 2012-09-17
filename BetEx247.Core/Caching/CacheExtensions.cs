f0be2b0d877f78c4deeda3ec68fa9c1abd2924c4
Phuc Ngo <hoangphucnguyen@yahoo.com>
2012-09-17 17:44:13 +0700

    Update new code

---
+++ BetEx247.Core/Caching/CacheExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BetEx247.Core.Caching
+{
+    /// <summary>
+    /// Extensions
+    /// </summary>
+    public static class CacheExtensions
+    {
+        public static T Get<T>(this ICacheManager cacheManager, string key, Func<T> acquire)
+        {
+            return Get(cacheManager, key, 60, acquire);
+        }
+
+        public static T Get<T>(this ICacheManager cacheManager, string key, int cacheTime, Func<T> acquire)
+        {
+            if (cacheManager.IsSet(key))
+            {
+                return cacheManager.Get<T>(key);
+            }
+            else
+            {
+                var result = acquire();
+                //if (result != null)
+                cacheManager.Set(key, result, cacheTime);
+                return result;
+            }
+        }
+    }
+}
